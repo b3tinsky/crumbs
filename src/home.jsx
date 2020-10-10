54b15ff383bfd031b16b315c9f50b454a6d2c11a
Beto Rendon <betorendonruiz@gmail.com>
2020-10-10 12:47:42 -0500

    Changed Title and Instructions color

--- src/home.jsx
+++ src/home.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import { Header, Grid, Container } from "semantic-ui-react";
 import styles from "./styles.module.scss";
-import Product from './Components/Product';
+import Product from "./Components/Product";
 export class Home extends Component {
   render() {
     return (
       <div className={styles.homeBg}>
-        <Header block style={{backgroundColor: "black"}}>
+        <Header block style={{ backgroundColor: "black" }}>
           <h1
             className={styles.header}
             style={{ fontFamily: "'Shrikhand', cursive" }}
@@ -15,18 +15,30 @@ export class Home extends Component {
           </h1>
         </Header>
         <Container>
-            <p className={styles.instrucciones}>
-                    Pedidos al <span style={{ color: 'black'}}>8180798541</span>. 1 Dia de anticipo. <br></br>(Hacer click en imagen para ver precios)
-            </p>
+          <p className={styles.instrucciones}>
+            Pedidos al <span style={{ color: "black" }}>8180798541</span>. 1 Dia
+            de anticipo. <br></br>(Hacer click en imagen para ver precios)
+          </p>
         </Container>
-        
-        <Grid container stackable columns={2} style={{paddingTop: '3%'}}>
+        <Grid container stackable columns={2} style={{ paddingTop: "3%" }}>
           <Grid.Column>
-            <Product price={'$200'} name={'Basque Burnt Cheesecake'} image={require('./Images/BasqueCheesecake.jpg')} />
-            <Product price={'Proximamente'} name={'Eclair'} image={require('./Images/Eclair.jpg')} />
+            <Product
+              price={"$200"}
+              name={"Basque Burnt Cheesecake"}
+              image={require("./Images/BasqueCheesecake.jpg")}
+            />
+            <Product
+              price={"Proximamente"}
+              name={"Eclair"}
+              image={require("./Images/Eclair.jpg")}
+            />
           </Grid.Column>
           <Grid.Column>
-            <Product price={'Proximamente'} name={'Rol de Canela'} image={require('./Images/CinnamonRoll.jpg')} />
+            <Product
+              price={"Proximamente"}
+              name={"Rol de Canela"}
+              image={require("./Images/CinnamonRoll.jpg")}
+            />
           </Grid.Column>
         </Grid>
       </div>
